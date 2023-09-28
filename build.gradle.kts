e3a9e271975a6fa4be12c2fcf8c15fd0bc1a27b0
Carlos Henrique <carlos_henrique1313@outlook.com>
2023-09-28 20:33:26 -0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(20)
+}
+
+group = "com.carloshenriquedev.library.catalog"
+version = "1.0-SNAPSHOT"
+

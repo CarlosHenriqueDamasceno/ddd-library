2710de4166d070ad019d1247c30da4c61caf33cf
Carlos Henrique <carlos_henrique1313@outlook.com>
2023-10-01 18:43:08 -0300

    adding author domain tests

---
+++ domain/src/test/kotlin/com/carloshenriquedev/library/catalog/domain/author/AuthorTest.kt
@@ -0,0 +1,40 @@
+package com.carloshenriquedev.library.catalog.domain.author
+
+import com.carloshenriquedev.library.catalog.domain.common.DomainException
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class AuthorTest {
+
+    @Test
+    fun `given valid parameters, should instantiate a new author`() {
+        // given
+        val expectedName = "Uncle Bob"
+
+        // when / Then
+        try {
+            Author.buildNewAuthor(expectedName)
+        } catch (exception: Throwable) {
+            fail(exception.message)
+        }
+    }
+
+    @Test
+    fun `given a empty name, should throw domain exception`() {
+        // given
+        val expectedName = "Uncle Bob"
+        val expectedErrorCount = 1
+        val expectedErrorMessage = "Author's name can not be empty."
+
+        // when / Then
+        try {
+            Author.buildNewAuthor(expectedName)
+        } catch (exception: Throwable) {
+            assertIs<DomainException>(exception)
+            assertEquals(expectedErrorCount, exception.handler.size)
+            assertEquals(expectedErrorMessage, exception.handler[0].message)
+        }
+    }
+}
